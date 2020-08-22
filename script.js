d76f781f3a76f866f8b53bb46fceab83be453b59
Greg Schuman <gcschuman@hotmail.com>
2020-08-22 10:51:18 -0700

    Added DOM elements to the script.

---
+++ script.js
@@ -0,0 +1,7 @@
+// DOM Elements
+const search = document.getElementById("search"),
+    submit = document.getElementById("submit"),
+    random  = document.getElementById("random"),
+    mealsEl = document.getElementById("meals"),
+    resultText = document.getElementById("result-text"),
+    single_mealEl = document.getElementById("single-meal");
