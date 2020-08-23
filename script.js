cee2c74244650a5cb5edd46fa277b4aa736fcf9e
Greg Schuman <gcschuman@hotmail.com>
2020-08-22 21:07:32 -0700

    Added the add meal to DOM function and the getRandomMeal event listener.

--- script.js
+++ script.js
@@ -8,6 +8,64 @@ const search = document.getElementById("search"),
 
 //Event listeners
 submit.addEventListener('submt', searchMeal);
+mealsEl.addEventListener('click', e =>{
+  const mealInfo = e.path.find(item => {
+    if(item.classList){
+      return item.classList.contains('meal-info');
+    } else{
+      return false
+    }
+  });
+
+  if(mealsInfo){
+    const mealId = mealInfo.getAttribute('data-mealid');
+    getMealById(mealId);
+  }
+})
+
+// Fetch meal by ID
+function getMealById(mealId){
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+  .then(res => res.json())
+  .then(data =>{
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  })
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+  `;
+}
 
 // Search meal and fetch from API
 function searchMeal(){
